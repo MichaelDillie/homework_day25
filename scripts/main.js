20d7ee83c77e7f5db2f0ec35684c91dd7e851e02
Michael Dillie <michaeldillie@gmail.com>
2015-10-05 10:58:15 -0500

    foundation

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+'use strict';
+var Backbone = require('backbone');
+var React = ('react');
+window.$ = require('jquery');
+window.jQuery = $;
+
+Parse.initialize('IiQzs9Nbc1ejEKzCCPOr5g4eQoCAvXxlOIJdwka3', 'dk6FNZcGQh1NQ5JEfJMU6Pyhmwx2ly6y4HjrBP1U');
+
+React.render(
+
+);
